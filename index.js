211842a5471d865c7244be38a8aa1963bc7bca7d
Kevin Harp <kharp1984@gmail.com>
2023-05-31 19:33:56 -0400

    jwt implemented

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 
+const { PORT, JWT_SECRET } = process.env;
 const express = require('express');
 const volleyball = require('volleyball');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const app = express();
 
 
@@ -13,19 +15,31 @@ let currentRegisteredUser = {};
 app.post('/register', async (req, res, next) => {
   try {
     const { username, password } = req.body;
+    const saltRound = 10;
     const hashedPassword = await bcrypt.hash(password, saltRound);
-    res.send(username, hashedPassword);
+    const token = jwt.sign({ username, password }, JWT_SECRET);
+    currentRegisteredUser = { username, password: hashedPassword };
+    res.send({ token, message: "Thanks for registering!" });
   } catch (error) {
     console.log(error);
   };
 });
 
-const { PORT } = process.env;
+app.post('/login', async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const isLoggedIn = await bcrypt.compare(password, currentRegisteredUser.password);
+    res.send({ isLoggedIn, registeredUser: currentRegisteredUser, user: req.body });
+  } catch (error) {
+    console.log(error);
+  };
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on port: ${ PORT }`);
 });
 
 
 //! Two methods with bcrypt:
-//! hash --> hashes a plaintext string/password
-//! compare  --> compares a plaintext string/password with a hashed string/password
+//! bcrypt.hash(<password/string>, saltRound); --> hashes a plaintext string/password (saltRound is variable set to a number (default 10));
+//! bcrypt.compare(<password/string>, <stored hash>);  --> compares a plaintext string/password with a hashed string/password
