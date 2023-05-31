1760c72d58074e7f877681d434fb91654bbd3829
Kevin Harp <kharp1984@gmail.com>
2023-05-31 19:03:18 -0400

    bcrypt applied

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 require('dotenv').config();
 
 const express = require('express');
-const app = express();
 const volleyball = require('volleyball');
-const { PORT } = process.env;
+const bcrypt = require('bcrypt');
+const app = express();
+
 
 app.use(volleyball);
+app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log(`Listening on port: ${ PORT }`)
+let currentRegisteredUser = {};
+app.post('/register', async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, saltRound);
+    res.send(username, hashedPassword);
+  } catch (error) {
+    console.log(error);
+  };
 });
+
+const { PORT } = process.env;
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${ PORT }`);
+});
+
+
+//! Two methods with bcrypt:
+//! hash --> hashes a plaintext string/password
+//! compare  --> compares a plaintext string/password with a hashed string/password
